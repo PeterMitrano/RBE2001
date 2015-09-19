b44aae6bf4d59969b5a8c96dabfe6457826100dc
Peter Mitrano <mitranopeter@gmail.com>
2015-09-19 04:29:10 -0400

    added basic framework for bluetooth

--- Final_Project/Bluetooth.cpp
+++ Final_Project/Bluetooth.cpp
@@ -1,1 +1,14 @@
 #include "Bluetooth.h"
+
+void BluetoothClient::setup(){
+  Serial3.begin(115200);
+}
+
+void BluetoothClient::readMessage(){
+  byte pkt[10];
+  if (btMaster.readPacket(pkt)){
+    if (pcol.getData(pkt,rawData,messageType)){
+      //switch on type?
+    }
+  }
+}
