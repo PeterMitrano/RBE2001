11358d2a9252eaccf678596e98ca8152e57d60fb
peter mitrano <mitranopeter@gmail.com>
2015-09-21 23:32:14 -0400

    removed some commands

--- Final_Project/GrabRod.cpp
+++
@@ -1,8 +0,0 @@
-#include "GrabRod.h"
-#include "CloseGripper.h"
-#include "BlinkLED.h"
-
-GrabRod::GrabRod() : CommandGroup("grab rod") {
-  addSequential(new CloseGripper());
-  addSequential(new BlinkLED());
-}
--- Final_Project/GrabRod.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "CommandGroup.h"
-#include "Robot.h"
-
-class GrabRod: public CommandGroup {
-  public:
-    GrabRod();
-};
