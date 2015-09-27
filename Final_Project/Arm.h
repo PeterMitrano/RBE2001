0f1d41c075d6f2cd6f5887eaca87d23c70f08fb2
peter mitrano <mitranopeter@gmail.com>
2015-09-27 17:57:12 -0400

    reconfigured docs and added some docs

--- Final_Project/Arm.h
+++ Final_Project/Arm.h
@@ -1,6 +1,6 @@
 #pragma once
-/* arm class
- * functions here control the 4 bar linkage
+/* \brief arm class
+ * functions here control the 4 bar linkage/slider crank
  */
 
 #include <Servo.h>
@@ -10,20 +10,20 @@
 class Arm {
   public:
 
-    /* initializes servos and other stuff. called by main arduino setup */
+    /* \brief initializes servos and other stuff. called by main arduino setup */
     void setup();
 
-    /* lower arm all the way to the top */
+    /* \brief lower arm all the way to the top */
     void down();
 
-    /* raise arm all the way to the bottom */
+    /* \brief raise arm all the way to the bottom */
     void up();
 
     void stop();
 
     void setPosition(int setpoint);
 
-    /* return position in encoder counts */
+    /* \brief return position in encoder counts */
     int position();
 
     Gripper *gripper;
@@ -32,7 +32,7 @@ class Arm {
 
     int motorPin = 10;
 
-    /* used by PI to computer integral */
+    /* \brief used by PI to computer integral */
     int integral;
 
     /* PI constants */
