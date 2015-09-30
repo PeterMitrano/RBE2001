29e6512c16b1c0b8e07f50eb7d3f58423bb78d4a
peter mitrano <mitranopeter@gmail.com>
2015-09-30 07:48:59 -0400

    added some nice commands to the calibrate routine

--- Final_Project/CalibrateRoutine.cpp
+++ Final_Project/CalibrateRoutine.cpp
@@ -3,9 +3,13 @@
 #include "TurnUntilLine.h"
 #include "Calibrate.h"
 #include "CalibrateArm.h"
+#include "RaiseArm.h"
+#include "OpenGripper.h"
 
 CalibrateRoutine::CalibrateRoutine(): CommandGroup("calibrate routine"){
   addParallel(new CalibrateArm());
-//  addSequential(new Calibrate());
-//  addSequential(new TurnUntilLine(2));
+  addSequential(new Calibrate());
+  addSequential(new TurnUntilLine(2));
+  addParallel(new OpenGripper());
+  addSequential(new RaiseArm());
 }
