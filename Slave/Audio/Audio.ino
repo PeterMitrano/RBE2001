6742e461d3dc5b9653a12666e266788f81a96122
peter mitrano <mitranopeter@gmail.com>
2015-10-03 05:28:03 -0400

    basic i2c code

--- Slave/Audio/Audio.ino
+++ Slave/Audio/Audio.ino
@@ -1,1 +1,15 @@
 //coprocessor code here
+#include <Wire.h>
+
+void setup(){
+  Wire.begin(1);
+  Wire.onReceive(dataEvent);
+}
+
+void loop(){}
+
+void dataEvent(){
+  if (Wire.available() ==  1){
+    int currrentCommand = Wire.read();
+  }
+}
