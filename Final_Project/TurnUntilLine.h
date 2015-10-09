1e0dc4a51aec83e6acfb7dfdc632ac1adc74bd1f
osh1996 <osh1996@gmail.com>
2015-10-09 13:47:20 -0400

    test commit for turnuntilline.h

--- Final_Project/TurnUntilLine.h
+++ Final_Project/TurnUntilLine.h
@@ -1,15 +1,39 @@
 #pragma once
-
+/** /brief 
+* turns until it hits a line
+*/
 #include "Command.h"
 #include "Robot.h"
 
 class TurnUntilLine : public Command {
   public:
+  	/** \brief 
+  	* turns until the line sensor is on line
+  	*/
     TurnUntilLine(int direction);
+
+    /** \brief 
+    * plays a song
+    */
     void initialize();
+
+    /** \brief
+    * rotates either ccw or cw
+    */
     void execute();
+
+    /** \returns
+    * true if robot is on line
+    */
     bool isFinished();
+
+    /** \brief
+    * stops robot
+    */
     void end();
   private:
+  	/** \brief
+    * this is the direction used to determine turn direction
+    */
     int direction;
 };
